3e8995e0c42fc8fd752d89cadbc2774cbea459ac
Scott Trinh <scott@scotttrinh.com>
2025-05-08 22:03:40 -0400

    No extraneous functionality

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NodeRuntime } from "@effect/platform-node"
-import { AutoThreadsLive } from "bot/AutoThreads"
-import { DadJokesLive } from "bot/DadJokes"
-import { DocsLookupLive } from "bot/DocsLookup"
 import { IssueifierLive } from "bot/Issueifier"
 import { NoEmbedLive } from "bot/NoEmbed"
-import { RemindersLive } from "bot/Reminders"
-import { ReproRequesterLive } from "bot/ReproRequester"
 import { Summarizer } from "bot/Summarizer"
 import { TracingLive } from "bot/Tracing"
 import { Config, Effect, Layer, LogLevel, Logger, RuntimeFlags } from "effect"
-import { PlaygroundLive } from "./Playground.js"
 import { ClusterLayer } from "./Cluster.js"
 
 const LogLevelLive = Layer.unwrapEffect(
@@ -21,15 +15,9 @@ const LogLevelLive = Layer.unwrapEffect(
 )
 
 const MainLive = Layer.mergeAll(
-  AutoThreadsLive,
   ClusterLayer,
-  DadJokesLive,
   NoEmbedLive,
-  DocsLookupLive,
   IssueifierLive,
-  PlaygroundLive,
-  RemindersLive,
-  ReproRequesterLive,
   Summarizer.Default,
 ).pipe(
   Layer.provide(TracingLive),
